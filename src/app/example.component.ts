33b8972c72805d242b016ba29305dab0608fd167
pkunszt <pkunszt@gmail.com>
2025-03-12 08:47:41 +0100

    update so it runs

--- src/app/example.component.ts
+++ src/app/example.component.ts
@@ -1,15 +1,16 @@
 import { Component } from '@angular/core';
-import { IonCheckbox, IonItem, IonList } from '@ionic/angular/standalone';
+import { IonCheckbox, IonIcon, IonItem, IonList } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-example',
   templateUrl: 'example.component.html',
   styleUrls: ['example.component.css'],
-  imports: [IonCheckbox, IonItem, IonList],
+  imports: [IonCheckbox, IonItem, IonList, IonIcon]
 })
 export class ExampleComponent {
   help(text: string, ev: MouseEvent) {
     ev.stopPropagation();
+    console.log(text);
     // show the help in a popup
     // the checkbox must not trigger
   }
